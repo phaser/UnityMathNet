2333fa30178692e57968122070c460d4b3a9e583
Cristian Bidea <cristian.bidea@qualitance.com>
2016-09-26 11:39:14 +0300

    Unity Math.Net working in Unity3d

---
+++ Assets/TestMathNet.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using MathNet.Numerics.LinearAlgebra;
+using MathNet.Numerics.LinearAlgebra.Double;
+
+public class TestMathNet : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        Matrix<double> A = DenseMatrix.OfArray(new double[,] {
+                                                                    {1,1,1,1},
+                                                                    {1,2,3,4},
+                                                                    {4,3,2,1}});
+        Debug.Log("Mat: " + A);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+Libraries used
+==============
+
+  * MathNet.Numerics.3.13.1
+  * TaskParallelLibrary.1.0.2856.0
